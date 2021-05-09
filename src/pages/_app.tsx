de74706f9f99169ec9cee0db6f90a07d20672368
Yash Mahalwal <yashmahalwal@gmail.com>
2021-05-10 01:59:32 +0530

    refactor: updated universal example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,29 @@
-import { AppProps } from "next/app";
-import { Suspense } from "react";
-import { RelayEnvironmentProvider } from "relay-hooks";
 import environment from "../relay/environment";
-function MyApp({ Component, pageProps }: AppProps) {
+import { AppProps } from "next/app";
+import { RelayEnvironmentProvider } from "relay-hooks";
+import { RecordMap } from "relay-runtime/lib/store/RelayStoreTypes";
+import React from "react";
+interface Props {
+    records?: RecordMap;
+}
+function MyApp({ Component, pageProps, records: r }: AppProps & Props) {
+    const records: RecordMap = React.useMemo(() => {
+        if (r) return r;
+        if (typeof document !== "undefined") {
+            const recordsData = document.getElementById("relay-data")
+                ?.innerHTML;
+            if (recordsData)
+                return JSON.parse(
+                    Buffer.from(recordsData, "base64").toString()
+                );
+        }
+        return {};
+    }, [r]);
+
     return (
+        <RelayEnvironmentProvider environment={environment(records)}>
             <Component {...pageProps} />
+        </RelayEnvironmentProvider>
     );
 }
 
